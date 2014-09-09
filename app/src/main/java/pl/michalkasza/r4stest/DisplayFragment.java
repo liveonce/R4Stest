8bc2e59bc979a552502f60b30b3aca9d661c8195
Michał Kasza <michalkasza@hotmail.com>
2014-09-09 15:55:21 +0200

    refreshing
    Added refreshing in DisplayFragment

--- app/src/main/java/pl/michalkasza/r4stest/DisplayFragment.java
+++ app/src/main/java/pl/michalkasza/r4stest/DisplayFragment.java
@@ -13,34 +13,42 @@ import java.util.ArrayList;
 
 import pl.michalkasza.r4stest.db.MySQLiteHelper;
 
-public class DisplayFragment extends Fragment implements View.OnClickListener{
+public class DisplayFragment extends Fragment {
     ListView listView ;
+    ArrayAdapter<String> adapter;
+    ArrayList link_list;
     MySQLiteHelper db;
     Button button;
+
 
     public DisplayFragment() {}
     @Override
     public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_display, container, false);
         button = (Button) rootView.findViewById(R.id.button);
-        button.setOnClickListener(this);
 
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                clearMethod();
+            }
+
+        });
         db = new MySQLiteHelper(getActivity());
-        ArrayList link_list = db.getAllLinks();
-        link_list.lastIndexOf("http://");
-
-
+        link_list = db.getAllLinks();
         listView = (ListView) rootView.findViewById(R.id.listView);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
-                R.layout.row, R.id.label, link_list);
+        adapter = new ArrayAdapter<String>(getActivity(),R.layout.row, R.id.label, link_list);
         listView.setAdapter(adapter);
 
         return rootView;
     }
 
-    @Override
-    public void onClick(View v) {
+    public void clearMethod() {
         db.deleteAll();
+        link_list = db.getAllLinks();
+        listView = (ListView) this.getActivity().findViewById(R.id.listView);
+        adapter.notifyDataSetChanged();
+        adapter = new ArrayAdapter<String>(getActivity(),R.layout.row, R.id.label, link_list);
+        listView.setAdapter(adapter);
     }
 }
